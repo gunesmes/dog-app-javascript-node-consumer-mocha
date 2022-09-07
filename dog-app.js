3d1e210688bd7267949beda52b3c3961b4748153
Mesut Güneş <gunesmes@gmail.com>
2022-09-07 10:58:14 +0300

    adding mock app and publish update

--- dog-app.js
+++ dog-app.js
@@ -30,3 +30,79 @@ exports.getMeDog = endpoint => {
     headers: { Accept: "application/json" },
   })
 }
+
+exports.getMeDogsWalk = endpoint => {
+  const url = endpoint.url
+  const port = endpoint.port
+
+  return axios.request({
+    method: "GET",
+    baseURL: `${url}:${port}`,
+    url: "/walk",
+    headers: { Accept: "application/json" },
+  })
+}
+
+// Gets a single entry by ID from the dog-sleep
+exports.getMeDogWalk = endpoint => {
+  const url = endpoint.url
+  const port = endpoint.port
+
+  return axios.request({
+    method: "GET",
+    baseURL: `${url}:${port}`,
+    url: "/walk/1",
+    headers: { Accept: "application/json" },
+  })
+}
+
+
+exports.getMeDogsRegister = endpoint => {
+  const url = endpoint.url
+  const port = endpoint.port
+
+  return axios.request({
+    method: "GET",
+    baseURL: `${url}:${port}`,
+    url: "/register",
+    headers: { Accept: "application/json" },
+  })
+}
+
+// Gets a single entry by ID from the dog-sleep
+exports.getMeDogRegister = endpoint => {
+  const url = endpoint.url
+  const port = endpoint.port
+
+  return axios.request({
+    method: "GET",
+    baseURL: `${url}:${port}`,
+    url: "/register/1",
+    headers: { Accept: "application/json" },
+  })
+}
+
+exports.getMeDogsPlay = endpoint => {
+  const url = endpoint.url
+  const port = endpoint.port
+
+  return axios.request({
+    method: "GET",
+    baseURL: `${url}:${port}`,
+    url: "/play",
+    headers: { Accept: "application/json" },
+  })
+}
+
+// Gets a single entry by ID from the dog-sleep
+exports.getMeDogPlay = endpoint => {
+  const url = endpoint.url
+  const port = endpoint.port
+
+  return axios.request({
+    method: "GET",
+    baseURL: `${url}:${port}`,
+    url: "/play/1",
+    headers: { Accept: "application/json" },
+  })
+}
